f6c398e73a1511757c648bf49a0c3b898645577f
EC2 Default User <ec2-user@ip-172-26-5-87.ap-northeast-1.compute.internal>
2023-02-16 04:12:41 +0000

    first

---
+++ index.js
@@ -0,0 +1,79 @@
+/**
+ * @file mofron-layout-marcent/index.js
+ * @brief margin-center layout module for mofron
+ * @license MIT
+ */
+const Margin    = require('mofron-layout-margin');
+const HrzCenter = require('mofron-layout-hrzcenter');
+
+module.exports = class extends mofron.class.Layout {
+    /**
+     * initialize layout
+     * 
+     * @param (mixed) 
+     *                key-value: layout config
+     * @short
+     * @type private
+     */
+    constructor (p1) {
+        try {
+            super();
+            this.modname('marcent');
+	    this.shortForm('top','rate');
+
+	    /* init config */
+            this.confmng().add('top',  { type: 'size', init:'0.2rem' });
+            this.confmng().add('rate', { type: 'number', init:80 });
+            
+	    if (0 < arguments.length) {
+                this.config(p1);
+	    }
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+
+    component (prm) {
+        try {
+            if (undefined !== prm) {
+                prm.layout([
+                    new Margin('top', this.top()),
+                    new HrzCenter(this.rate())
+                ]);
+            }
+            return super.component(prm);
+	} catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    /**
+     * layout contents
+     * 
+     * @param (number) target child index
+     * @param (component) target component object
+     * @type private
+     */
+    contents (idx, cmp) {}
+
+    top (prm) {
+        try {
+            return this.confmng('top', prm);
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+
+    rate (prm) {
+        try {
+            return this.confmng('rate', prm);
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+}
+/* end of file */
